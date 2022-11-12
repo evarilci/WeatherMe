8e02aeaad63b0297170f7e526950f0452798538a
Eymen Varilci <evarilci@gmail.com>
2022-11-12 22:31:18 +0300

    Initial commit

---
+++ WeatherMe/WeatherMe/Screens/Initial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherMe
+//
+//  Created by Eymen Varilci on 12.11.2022.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
